4b6ddce04a86b57a1e12dc079848d1d380033f80
DURGA RAJA <rajakotha5785@gmail.com>
2023-10-21 08:11:11 +0530

    changed third page

--- lib/third.dart
+++ lib/third.dart
@@ -11,24 +11,75 @@ class third extends StatefulWidget {
 class _thirdState extends State<third> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(backgroundColor: Color(0xFF2d28b8),title: Text("Departments"),),body:Container(
-      decoration: BoxDecoration(
-          gradient: LinearGradient(begin: Alignment.topLeft,end: Alignment.bottomRight,
-              colors: [Colors.blue,Colors.white60,Colors.blue])
-      ),
-      child: ListView(children: [
-        ListTile(title: Text("Computer Science\n Engineering"),onTap: (){},),
-        Divider(color: Color(0xff2d28b8),thickness: 1,),
-        ListTile(title: Text("Electronic&Communication\n Engineering"),),
-        Divider(color: Color(0xff2d28b8),thickness: 1,),
-        ListTile(title: Text("Electronics&Electrical\n Engineering"),),
-        Divider(color: Color(0xff2d28b8),thickness: 1,),
-        ListTile(title: Text("Mechanical\n Engineering"),),
-        Divider(color: Color(0xff2d28b8),thickness: 1,),
-        ListTile(title: Text("Civil\n Engineering"),),
-        Divider(color: Color(0xff2d28b8),thickness: 1,),
-        ListTile(title: Text("Information\n Technology"),),
-        Divider(color: Color(0xff2d28b8),thickness: 1,),
+    return Scaffold(body:Padding(
+      padding: const EdgeInsets.fromLTRB(16,60, 16,0),
+      child: GridView(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 25,
+        mainAxisSpacing: 25
+      ),children: [
+        Container(
+          height: 200,width: 200,decoration: BoxDecoration(color: Colors.white, boxShadow: [BoxShadow(
+          color: Colors.grey,blurRadius: 10,spreadRadius: 5
+        )],
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.circular(30),
+
+
+        ),
+        ),
+        Container(
+          height: 200,width: 200,decoration: BoxDecoration(color: Colors.white, boxShadow: [BoxShadow(
+            color: Colors.grey,blurRadius: 10,spreadRadius: 5
+        )],
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.circular(30),
+
+
+        ),
+        ),
+        Container(
+          height: 200,width: 200,decoration: BoxDecoration( color: Colors.white,boxShadow: [BoxShadow(
+            color: Colors.grey,blurRadius: 10,
+          spreadRadius: 5
+        )],
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.circular(30),
+
+
+        ),
+        ),
+        Container(
+          height: 200,width: 200,decoration: BoxDecoration( color: Colors.white,boxShadow: [BoxShadow(
+            color: Colors.grey,blurRadius: 10,
+          spreadRadius: 5
+        )],
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.circular(30),
+
+
+        ),
+        ),
+        Container(
+          height: 200,width: 200,decoration: BoxDecoration( color: Colors.white,boxShadow: [BoxShadow(
+            color: Colors.grey,blurRadius: 10,spreadRadius: 5
+        )],
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.circular(30),
+
+
+        ),
+        ), Container(
+          height: 200,width: 200,decoration: BoxDecoration( color: Colors.white,boxShadow: [BoxShadow(
+            color: Colors.grey,blurRadius: 10,
+          spreadRadius: 5
+        )],
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.circular(30),
+
+
+        ),
+        )
       ],),
     ));
   }
